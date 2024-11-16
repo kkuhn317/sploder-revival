8cb358a6cf5a2893077265fe3ad76dfc9ba88f63
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2024-11-16 17:13:52 +0530

    Disallow comments

--- src/games/moderation/php/unban.php
+++ src/games/moderation/php/unban.php
@@ -3,15 +3,9 @@
 include('verify.php');
 $username = $_REQUEST['username'];
 
-// Check whether user has not been banned
-$sql = "SELECT COUNT(*) FROM banned_members WHERE username=:username";
-$statement = $db->prepare($sql);
-$statement->execute([
-    ':username'=>$username
-]);
-$count = $statement->fetchColumn();
-if($count==0){
-    header("Location: ../index.php?err=User is not banned");
+include('../content/checkban.php');
+if(!checkBan($username)){
+    header("Location: ../index.php?err=User not banned");
     die();
 }
 
