db4034b2f9ee31c4b75289119fe9aedf99a08daf
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2024-11-16 16:44:36 +0530

    Add moderation panel with backend

---
+++ src/games/moderation/content/subnav.php
@@ -0,0 +1,11 @@
+<div id="subnav">                
+                <ul class="nav_games">
+                <li><a href="/">Home</a></li>
+                <li><a href="index.php">Moderation</a></li>
+                <li><a href="pending.php">Pending deletion</a></li>
+                <li><a href="ipcheck.php">IP check</a></li>
+                <li><a href="banned.php">Banned members</a></li>
+                
+
+			    </ul>
+</div>
---
+++ src/games/moderation/pending.php
@@ -0,0 +1,92 @@
+<?php include('php/verify.php'); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN" "http://www.w3.org/MarkUp/DTD/xhtml-rdfa-1.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php include('../../content/head.php'); ?>
+
+<link rel="stylesheet" type="text/css"  href="/css/sploder_v2p22.min.css" />
+<script>function delproj(id,title) {
+  if (confirm(("Are you sure you want to delete "+title)) == true) {
+    location.href = ("php/delete.php?return=pending.php&url=h://a/a/a.a?id="+id);  } else {}
+  }</script>
+	
+	<?php include('../../content/onlinecheck.php'); ?>
+
+</head>
+<?php include('../../content/addressbar.php'); ?>
+<body id="everyones" class="boosts" >
+<?php include('../../content/headernavigation.php'); ?>
+
+		<div id="page">
+        <?php include('content/subnav.php'); ?>
+
+        <div id="content" style="width:940px;">
+        <?php if(isset($_GET['err'])): ?>
+        <p class = "alert"><?= htmlspecialchars($_GET['err']) ?></p>
+        <?php endif; ?>
+        <?php if(isset($_GET['msg'])): ?>
+        <p class = "prompt" ><?= htmlspecialchars($_GET['msg']) ?></p>
+        <?php endif; ?>
+        
+        <h2>Games pending deletion</h2>
+        <p>Games that have been inactive for 14 days will be removed from this list.</p>
+
+        <?php
+        // Remove pending deletions older than 14 days
+        $sql = "DELETE FROM pending_deletions WHERE timestamp < NOW() - INTERVAL '14 days'";
+        $statement = $db->prepare($sql);
+        $statement->execute();
+        // Get pending games and display name, thumbnail, username
+        $sql = "SELECT games.g_id, games.date, MIN(pending_deletions.timestamp) as deletion_date, g_swf, author, title, userid, reason, views 
+          FROM pending_deletions
+          JOIN games ON games.g_id = pending_deletions.g_id 
+          JOIN members ON games.author = members.username 
+          WHERE pending_deletions.timestamp = (SELECT MIN(timestamp) FROM pending_deletions pd WHERE pd.g_id = games.g_id)
+          GROUP BY games.g_id, g_swf, author, title, userid, reason, views";
+        $statement = $db->prepare($sql);
+        $statement->execute();
+        $games = $statement->fetchAll();
+          
+        
+        ?>
+        <?php if(count($games) != 0): ?>
+        <div id="viewpage">
+        <div style="width:915px;" class="set">
+          <?php foreach($games as $game): ?>
+            
+            <div class="game">
+              <div class="photo">
+              <a href="/games/play.php?&id=<?= $game['g_id'] ?>&g_swf=<?= $game['g_swf'] ?>&title=<?= $game['title'] ?>&pub=0">
+                <img src="/users/user<?= $game['userid'] ?>/images/proj<?= $game['g_id'] ?>/thumbnail.png" width="80" height="80"/>
+              </a>
+              </div>
+              <p class="gamedate"><?= date('m&\m\i\d\d\o\t;d&\m\i\d\d\o\t;y', strtotime($game['date'])) ?></p>
+              <h4>
+                <a href="/games/play.php?&id=<?= $game['g_id'] ?>&g_swf=<?= $game['g_swf'] ?>&title=<?= $game['title'] ?>&pub=0"><?= urldecode($game['title']) ?></a>
+              </h4>
+              <h5>
+                <a href="../../members/index.php?u=<?= $game['author'] ?>"><?= $game['author'] ?></a>
+              </h5>
+              <p class="gameviews"><?= $game['views'] ?> views</p>
+              <input title="Delete" type="button" onclick="delproj(<?= $game['g_id'] ?>,'<?= urldecode($game['title']) ?>')" style="width:37px" value="Delete">&nbsp;
+              <br><br>
+              <?= nl2br(htmlspecialchars($game['reason'])) ?>
+            </div>
+                     
+          <?php endforeach; ?>
+        <div class="spacer">&nbsp;</div>
+        </div>
+        
+
+        </div>
+        <?php else:
+        echo "<p>No games pending deletion</p>";
+        endif; ?>
+          
+<div class="spacer">&nbsp;</div></div>
+			
+			<div class="spacer">&nbsp;</div>
+			<?php include('../../content/footernavigation.php'); ?>
+</body>
+</html>
