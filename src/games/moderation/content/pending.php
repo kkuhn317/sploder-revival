586b06b6ba490d6251a4f14bbd0083f4c68f76a8
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2024-11-21 10:47:00 +0530

    Revert "Fix db err"
    This reverts commit 591264c0b1fa9b9286e5b3d655c62d35bc01bed0.

--- src/games/moderation/content/pending.php
+++ src/games/moderation/content/pending.php
@@ -1,11 +1,15 @@
 <?php
 // Remove pending deletions older than 14 days
-        $db->query("DELETE FROM pending_deletions WHERE timestamp < NOW() - INTERVAL '14 days'");
+        $sql = "DELETE FROM pending_deletions WHERE timestamp < NOW() - INTERVAL '14 days'";
+        $statement = $db->prepare($sql);
+        $statement->execute();
         // Get pending games and display name, thumbnail, username
-        $games = $db->query("SELECT games.g_id, games.date, MIN(pending_deletions.timestamp) as deletion_date, g_swf, author, title, userid, reason, views 
+        $sql = "SELECT games.g_id, games.date, MIN(pending_deletions.timestamp) as deletion_date, g_swf, author, title, userid, reason, views 
           FROM pending_deletions
           JOIN games ON games.g_id = pending_deletions.g_id 
           JOIN members ON games.author = members.username 
           WHERE pending_deletions.timestamp = (SELECT MIN(timestamp) FROM pending_deletions pd WHERE pd.g_id = games.g_id)
-          GROUP BY games.g_id, g_swf, author, title, userid, reason, views");
-        
+          GROUP BY games.g_id, g_swf, author, title, userid, reason, views";
+        $statement = $db->prepare($sql);
+        $statement->execute();
+        $games = $statement->fetchAll();
