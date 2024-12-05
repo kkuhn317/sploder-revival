22bc03cb5d3bdce20e389c832a0cc2365bf33a32
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2024-12-05 17:32:30 +0530

    Remove dead code

--- src/graphics/put.php
+++ src/graphics/put.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
-//error_reporting(E_ALL);
-//ini_set('display_errors', 1);
 header('Content-Type: text/xml');
 $rawdata = file_get_contents("php://input");
 $id = $_REQUEST['projid'];
