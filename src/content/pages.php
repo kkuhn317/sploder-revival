f5a850ad6f67b602ea408f34a6230ce388fdc50a
Sploder-Saptarshi <85979829+Sploder-Saptarshi@users.noreply.github.com>
2024-12-07 09:39:52 +0530

    Update database connection and tidy URL for pagination

--- src/content/pages.php
+++ src/content/pages.php
@@ -1,6 +1,10 @@
 <div class="pagination">
     <?php
     $params = $_SERVER['QUERY_STRING'];
+    parse_str($params, $queryParams);
+    unset($queryParams['o']);
+    $queryString = http_build_query($queryParams);
+
     $perPage = 12;
     $totalPages = ceil($total / $perPage);
     $currentPage = isset($_GET['o']) ? (int)$_GET['o'] / $perPage + 1 : 1;
@@ -8,7 +12,7 @@
     $end = min($totalPages, $start + 6);
     if ($total != 0) {
         if ($currentPage < $totalPages) {
-            echo '<a href="?' . $params . '&o=' . ($currentPage * $perPage) . '" class="page_button page_next">next &raquo;</a>';
+            echo '<a href="?' . $queryString . '&o=' . ($currentPage * $perPage) . '" class="page_button page_next">next &raquo;</a>';
         } else {
             echo '<span style="width:52px" class="page_button page_next">next &raquo;</span>';
         }
@@ -16,11 +20,11 @@
             if ($i == $currentPage) {
                 echo '<span class="page_button page_current">' . $i . '</span>';
             } else {
-                echo '<a href="?' . $params . '&o=' . (($i - 1) * $perPage) . '" class="page_button">' . $i . '</a>';
+                echo '<a href="?' . $queryString . '&o=' . (($i - 1) * $perPage) . '" class="page_button">' . $i . '</a>';
             }
         }
         if ($currentPage > 1) {
-            echo '<a href="?' . $params . '&o=' . ((($currentPage - 2) * $perPage) - 12) . '" class="page_button">&laquo;</a>';
+            echo '<a href="?' . $queryString . '&o=' . ((($currentPage - 2) * $perPage) - 12) . '" class="page_button">&laquo;</a>';
         } else {
             echo '<span class="page_button page_button_inactive">&laquo;</span>';
         }
