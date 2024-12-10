a12e9bbbf6edd6bbd3f3de2c8841fb0131f3a5db
Sploder-Saptarshi <85979829+Sploder-Saptarshi@users.noreply.github.com>
2024-12-10 17:59:06 +0530

    Add ratings

---
+++ src/games/content/newest.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+// Get required data...
+require_once('../database/connect.php');
+$db = getDatabase();
+$qs = "SELECT g.author, g.title, g.description, g.g_id, g.user_id, g.g_swf, g.date, g.user_id, g.views, 
+ROUND(AVG(r.score), 1) as avg_rating, COUNT(r.score) as total_votes 
+FROM games g 
+LEFT JOIN votes r ON g.g_id = r.g_id 
+WHERE g.ispublished = 1 AND g.isprivate = 0 
+GROUP BY g.g_id 
+ORDER BY g.g_id DESC 
+LIMIT 12";
+$games = $db->query($qs);
+$perPage = 12;
+$qs = "SELECT COUNT(g_id) FROM games WHERE ispublished = 1 AND isprivate = 0";
+$total = $db->queryFirstColumn($qs);
+$currentpage = 'newest.php';
+$gamesCount = count($games);
