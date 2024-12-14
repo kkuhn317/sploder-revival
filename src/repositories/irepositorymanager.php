8ffb70e523ba7fcecb1fc34f7fdc83cac89526e3
Grant Lanham Jr <contact@grantlanham.com>
2024-12-14 17:54:38 -0500

    Add the most common repositories to the project and create unit tests (#89)

--- src/repositories/irepositorymanager.php
+++ src/repositories/irepositorymanager.php
@@ -1,8 +1,17 @@
 <?php
 
 require_once(__DIR__ . "/igamerepository.php");
+require_once(__DIR__ . "/igraphicsrepository.php");
+require_once(__DIR__ . "/iuserrepository.php");
+require_once(__DIR__ . "/iawardsrepository.php");
+require_once(__DIR__ . "/iuserrepository.php");
+require_once(__DIR__ . "/icontestrepository.php");
 
 interface IRepositoryManager
 {
+    public function getAwardsRepository(): IAwardsRepository;
+    public function getContestRepository(): IContestRepository;
     public function getGameRepository(): IGameRepository;
+    public function getGraphicsRepository(): IGraphicsRepository;
+    public function getUserRepository(): IUserRepository;
 }
