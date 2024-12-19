e6644638a5dae4872ded8a98cfb9c8be10912d21
Sploder-Saptarshi <85979829+Sploder-Saptarshi@users.noreply.github.com>
2024-12-19 10:30:57 +0530

    Fix weird javascript... Who did this(?)

--- src/content/onlinechecker.php
+++ src/content/onlinechecker.php
@@ -3,13 +3,13 @@ if (isset($_SESSION['username'])) {
     if (!isset($status)) {
         $status = "online";
     }
-    ?>
-<script>
-fetch(__DIR__."/../php/idlecheck.php")
-function checkonline() 
-{
-    fetch(__DIR__."/../php/online.php?status=<?= $status ?>");
-}
-var checkonline = setInterval(checkonline, 10000);
-</script>
+?>
+    <script>
+        fetch("/php/idlecheck.php")
+
+        function checkonline() {
+            fetch("/php/online.php?status=<?= $status ?>");
+        }
+        var checkonline = setInterval(checkonline, 10000);
+    </script>
 <?php } ?>
