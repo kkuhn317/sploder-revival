52cb54ca451df2dd976406b756c0d2649711102d
Grant Lanham Jr <contact@grantlanham.com>
2024-12-23 16:12:01 -0500

    Move accounts/config.php configuration to .env (#22)

--- src/accounts/config.php
+++ src/accounts/config.php
@@ -1,27 +1,16 @@
 <?php
+require_once("../config/env.php");
 
-# CLIENT ID
-# https://i.imgur.com/GHI2ts5.png (screenshot)
-$client_id = "1018420155690270771";
+$client_id = getenv("DISCORD_CLIENT_ID");
 
-# CLIENT SECRET
-# https://i.imgur.com/r5dYANR.png (screenshot)
-$secret_id = "p5Dzz5p8oVwWU3AM4c8ETfBUmXnX6PJf";
+$secret_id = getenv("DISCORD_SECRET_ID");
 
-# SCOPES SEPARATED BY + SIGN
-# example: identify+email+guilds+connections
-# $scopes = "identify+email";
-$scopes = "identify";
+$scopes = getenv("DISCORD_SCOPES");
 
-# REDIRECT URL
-# example: https://mydomain.com/includes/login.php
-# example: https://mydomain.com/test/includes/login.php
-$redirect_url = "https://sploder.xyz/accounts/checkmigrationownership.php";
+$redirect_url = getenv("DISCORD_REDIRECT_URL");
 
 # IMPORTANT READ THIS:
 # - Set the `$bot_token` to your bot token if you want to use guilds.join scope to add a member to your server
 # - Check login.php for more detailed info on this.
 # - Leave it as it is if you do not want to use 'guilds.join' scope.
-
-# https://i.imgur.com/2tlOI4t.png (screenshot)
-$bot_token = "MTAxODQyMDE1NTY5MDI3MDc3MQ.GUEYqY.vlQugs7z3zb_JxSHqoHUcTBlXdU55DeCfc71CY";
+$bot_token = getenv("DISCORD_BOT_TOKEN");
