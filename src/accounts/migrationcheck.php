e9de771a9637eee06746dbae9c003e9bd6d212cf
Grant Lanham Jr <contact@grantlanham.com>
2024-12-28 17:24:45 -0500

    Migrate SQLite usage to the Database wrapper and .env variable (#100)

--- src/accounts/migrationcheck.php
+++ src/accounts/migrationcheck.php
@@ -1,26 +1,26 @@
 <?php
 
-session_Start();
+session_start();
 session_destroy();
-session_Start();
+session_start();
+
+require_once("../database/connect.php");
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 $u = mb_strtolower($_POST['username']);
-$db = new PDO('sqlite:../database/originalmembers.db');
-#$qs = "UPDATE sploder SET isprivate=" . $isprivate . " WHERE g_id = " . $id;
-$qs2 = "SELECT username FROM members WHERE username=:user LIMIT 1";
-$statement2 = $db->prepare($qs2);
-$statement2->execute([':user' => $u]);
 
-$result2 = $statement2->fetchAll();
-include_once('../database/connect.php');
-$db = connectToDatabase('members');
+$originalMembersDb = getOriginalMembersDatabase();
 #$qs = "UPDATE sploder SET isprivate=" . $isprivate . " WHERE g_id = " . $id;
-$qs2 = "SELECT username FROM members WHERE username=:user LIMIT 1";
-$statement2 = $db->prepare($qs2);
-$statement2->execute([':user' => $u]);
-$result3 = $statement2->fetchAll();
+$result2 = $originalMembersDb->query("SELECT username FROM members WHERE username=:user LIMIT 1", [
+    ':user' => $u
+]);
+
+$db = getDatabase();
+$result3 = $db->query("SELECT username FROM members WHERE username=:user LIMIT 1", [
+    ':user' => $u
+]);
+
 if (isset($result3[0]['username'])) {
     $status1 = "cant";
 } else {
@@ -33,9 +33,9 @@ if ($status1 == "can") {
         $status = "green";
     }
     if ($status == "alert") {
-        require __DIR__ . "/functions.php";
-        require __DIR__ . "/discord.php";
-        require __DIR__ . "/config.php";
+        require(__DIR__ . "/functions.php");
+        require(__DIR__ . "/discord.php");
+        require(__DIR__ . "/config.php");
         $auth_url = url($client_id, $redirect_url, $scopes);
         $_SESSION['enteredusername'] = $u;
         header('Location: ' . $auth_url);
