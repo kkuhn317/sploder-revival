6bb5c528681501362fe13ca90f19daf68665e20f
Sploder-Saptarshi <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-01-02 23:08:31 +0530

    Update invite and SWF

--- src/content/footernavigation.php
+++ src/content/footernavigation.php
@@ -11,6 +11,6 @@
         <li><a href="/legal/termsofservice.php">Terms of Service</a></li>
         <li><a href="/credits.php">Credits</a></li>
         <li><a href="/legal/privacypolicy.php">Privacy Policy</a></li>
-        <li><a href="https://discord.gg/E9RmT3TXBa" target="_blank">Discord</a></li>
+        <li><a href="https://discord.com/invite/cHGz362sdC/" target="_blank">Discord</a></li>
     </ul><br><br><i>All assets are the property of neurofuzzy</i>
 </div>
