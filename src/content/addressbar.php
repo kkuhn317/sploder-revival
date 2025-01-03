9ce6856798a878f4c5050d0224d3795d1a97ab0f
Sploder-Saptarshi <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-01-03 19:47:18 +0530

    Move css

--- src/content/addressbar.php
+++ src/content/addressbar.php
@@ -3,48 +3,6 @@ if (str_contains($_SERVER['HTTP_USER_AGENT'], 'Electron')) {
     require_once(__DIR__ . '/../config/env.php');
     $domainName = getenv("DOMAIN_NAME");
 ?>
-<style>
-.topnav .search-container {
-    float: none !important;
-    border-top: 0px !important;
-    margin: 0px !important;
-}
-
-.topnav a,
-.topnav input[type=text],
-.topnav .search-container button,
-.urlmessage {
-    border-top: 0px !important;
-    border: 5px solid !important;
-    transform: translate(20%, 0) !important;
-    padding: 10px !important;
-    float: top !important;
-    display: block !important;
-    justify-content: center !important;
-    text-align: left !important;
-    width: 70% !important;
-    border-top: none !important;
-    outline-width: 0 !important;
-    position: fixed !important;
-    background: white !important;
-    color: #000000 !important;
-    z-index: 99999 !important;
-
-}
-
-.topnav input[type=text],
-.urlmessage {
-
-    border: 1px solid #ccc !important;
-    font-size: 16px;
-    height: 23px;
-    line-height: 18px;
-    margin-top: -5px;
-    margin: 0;
-    font-family: "courier new", monospace;
-    font-weight: bold;
-}
-</style>
 
 <div class="topnav">
 
