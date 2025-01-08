2725c1b169590e8f5a1d42b131842deea268e7c6
Sploder-Saptarshi <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-01-08 18:13:42 +0530

    Fix original sploder arcade pause bug

--- src/make/arcade.php
+++ src/make/arcade.php
@@ -51,6 +51,24 @@
 
     disable_scroll();
 
+    // Make the flashcontent think that there is always a mouse cursor inside the window
+    // This is done by blocking events of the mouse outside the flashwindow to be sent to the flashwindow
+
+    function blockMouseEvents(e) {
+    var flashcontent = document.getElementById("flashcontent");
+    var flashcontentRect = flashcontent.getBoundingClientRect();
+    var mouseX = e.clientX;
+    var mouseY = e.clientY;
+    if (mouseX < flashcontentRect.left || mouseX> flashcontentRect.right || mouseY < flashcontentRect.top || mouseY>
+        flashcontentRect.bottom) {
+            e.preventDefault();
+        }
+    }
+
+    document.addEventListener("mousedown", blockMouseEvents, true);
+    document.addEventListener("mouseup", blockMouseEvents, true);
+
+
     swfobject.embedSWF("/swf/creator7preloader2.swf", "flashcontent", "860", "626", "10.2.152",
     "/swfobject/expressInstall.swf", flashvars, params);
     </script>
