6a8e6ce5fc4f4856161242c91a23462819094257
Grant Lanham Jr <contact@grantlanham.com>
2025-01-08 07:01:47 -0500

    Update bootstrap to insert multiple dummy games (#134)

--- db/bootstrap.sh
+++ db/bootstrap.sh
@@ -14,14 +14,25 @@ psql -U sploder -d sploder --command="\
   insert into members (username, password, joindate, lastlogin, perms, boostpoints, lastpagechange, level, ip_address, isolate)\
   values ('test2', '\$2y\$10\$rLev0HX15rWPzp8j928yG.2uL9DGWYLcr8fxBnJmYyaeFPogxdiI2', 1733445270, 1733445270, 'MRE', 250, 0, 1000, '127.0.0.1', true);"
 
-# DUMMY GAME
-psql -U sploder -d sploder --command="\
-  insert into games (author, title, date, description, g_swf, user_id)\
-  values ('test', 'gtest', timezone('utc', now()), 'foobar', 0, 1);"
+# DUMMY GAMES
+for i in $(seq 1 50);
+do
+    psql -U sploder -d sploder --command="\
+        insert into games (author, title, date, description, g_swf, user_id, isprivate, ispublished)\
+        values ('test', 'gtest$i', timezone('utc', now()), 'foobar', 0, 1, 0, 1);"
 
-psql -U sploder -d sploder --command="\
-  insert into game_tags (g_id, tag)\
-  values (1, 'foobar');"
+    psql -U sploder -d sploder --command="\
+        insert into game_tags (g_id, tag)\
+        values ($i, 'foobar');"
+done
+
+# DUMMY DELETIONS
+for i in $(seq 1 20);
+do
+    psql -U sploder -d sploder --command="\
+        insert into pending_deletions (g_id, deleter, reason, timestamp)
+        values ($i, 'test', 'stuff$i', timezone('utc', now()));"
+done
 
 # DUMMY GRAPHIC
 psql -U sploder -d sploder --command="\
