b3d58b35fe160175c345b50c358179ead621189c
Sploder-Saptarshi <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-03-07 13:48:36 +0530

    Fix leaderboards and avatars (Geoff, you are not safe for your crimes)

--- src/php/avatarproxy.php
+++ src/php/avatarproxy.php
@@ -1,10 +1,31 @@
 <?php
-
+header('Content-Type: image/png');
 $username = $_GET['u'];
 // Check file exists php
 if (file_exists('../avatar/a/' . $username . '.png')) {
     $raw = file_get_contents('../avatar/a/' . $username . '.png');
-    echo $raw;
 } else {
-    echo file_get_contents('../avatar/a/fb/noob.png');
+    $raw = file_get_contents('../avatar/a/fb/noob.png');
 }
+
+// Allow only few sizes
+$size = $_GET['size'] ?? 96;
+if ($size != 96 && $size != 48 && $size != 24) {
+    $size = 96;
+}
+// Resize the image if its not 96x96
+if ($size != 96) {
+    $img = imagecreatefromstring($raw);
+    $img = imagescale($img, $size, $size);
+
+    // Preserve transparency
+    imagesavealpha($img, true);
+    $transparent = imagecolorallocatealpha($img, 0, 0, 0, 127);
+    imagefill($img, 0, 0, $transparent);
+
+    ob_start();
+    imagepng($img);
+    $raw = ob_get_clean();
+}
+echo $raw;
+?>
