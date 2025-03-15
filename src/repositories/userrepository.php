6a4093190f8287bc3e216459691f3b37c7a3058a
Sploder-Saptarshi <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-03-15 12:10:08 +0530

    add sql query

--- src/repositories/userrepository.php
+++ src/repositories/userrepository.php
@@ -28,4 +28,33 @@ DESC LIMIT :limit";
         ':limit' => $limit,
         ]);
     }
+
+    public function getTopMembers()
+    {
+        $qs = "
+        WITH latest_view AS (
+    SELECT MAX(create_date) AS max_time
+    FROM (
+        SELECT MAX(create_date) AS create_date FROM public.game_views_members
+        UNION ALL
+        SELECT MAX(create_date) FROM public.game_views_anonymous
+    ) AS combined_views
+)
+SELECT 
+    g.author, 
+    COUNT(gv.g_id) + COUNT(ga.g_id) AS total_views, 
+    (SELECT max_time FROM latest_view) AS last_view_time
+FROM public.games g
+LEFT JOIN public.game_views_members gv 
+    ON g.g_id = gv.g_id 
+    AND gv.create_date >= (SELECT max_time FROM latest_view) - INTERVAL '24 hours'
+LEFT JOIN public.game_views_anonymous ga 
+    ON g.g_id = ga.g_id 
+    AND ga.create_date >= (SELECT max_time FROM latest_view) - INTERVAL '24 hours'
+GROUP BY g.author
+ORDER BY total_views DESC
+LIMIT 90;
+";
+        return $this->db->query($qs);
+    }
 }
