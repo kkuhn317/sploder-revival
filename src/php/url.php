a859bf49f9076c54aa313f5e1814baa320e6adc1
Sploder-Saptarshi <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-03-16 20:15:40 +0530

    Fix URL

--- src/php/url.php
+++ src/php/url.php
@@ -5,18 +5,22 @@ require_once(__DIR__ . '/../config/env.php');
 $domainName = getenv("DOMAIN_NAME");
 $domain = substr($url, 0, strlen($domainName));
 $back = str_replace("&urlerr=1", "", str_replace("&err404=1", "", $_POST["back"]));
-$file = $url;
-$file_headers = @get_headers($file);
-if (!$file_headers || $file_headers[0] !== 'HTTP/1.1 200 OK') {
+
+// Extract the file path from the URL
+$parsedUrl = parse_url($url);
+$file = $parsedUrl['path'];
+$exists = file_exists(__DIR__."/../".$file);
+
+if (!$exists) {
     $err404 = true;
 } else {
     $err404 = false;
 }
-echo $file_headers;
+
 if (($domain == $domainName) && ($err404 == false)) {
     header("Location: " . $url);
 } elseif ($domain !== $domainName) {
     header("Location: " . $back . "&urlerr=1");
-} elseif ($err404 == true) {
+} elseif ($err404) {
     header("Location: " . $back . "&errload=1");
 }
