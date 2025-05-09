0f23889a0bcd307517fe37faf6bcdcd6e244639b
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-05-09 14:03:15 +0530

    Update bootstrap.sh

--- db/bootstrap.sh
+++ db/bootstrap.sh
@@ -9,11 +9,11 @@ psql -U sploder -d sploder -f /bootstrap/sploder.sql
 
 # TEST USERS
 psql -U sploder -d sploder --command="\
-  insert into members (username, password, joindate, lastlogin, perms, boostpoints, lastpagechange, level, ip_address, isolate)\
-  values ('test', '\$2y\$10\$rLev0HX15rWPzp8j928yG.2uL9DGWYLcr8fxBnJmYyaeFPogxdiI2', 1733445270, 1733445270, 'MRE', 250, 0, 1000, '127.0.0.1', true);"
+  insert into members (username, password, joindate, lastlogin, perms, boostpoints, lastpagechange, ip_address, isolate)\
+  values ('test', '\$2y\$10\$rLev0HX15rWPzp8j928yG.2uL9DGWYLcr8fxBnJmYyaeFPogxdiI2', 1733445270, 1733445270, 'MRE', 250, 1000, '127.0.0.1', true);"
 psql -U sploder -d sploder --command="\
-  insert into members (username, password, joindate, lastlogin, perms, boostpoints, lastpagechange, level, ip_address, isolate)\
-  values ('test2', '\$2y\$10\$rLev0HX15rWPzp8j928yG.2uL9DGWYLcr8fxBnJmYyaeFPogxdiI2', 1733445270, 1733445270, 'MRE', 250, 0, 1000, '127.0.0.1', true);"
+  insert into members (username, password, joindate, lastlogin, perms, boostpoints, lastpagechange, ip_address, isolate)\
+  values ('test2', '\$2y\$10\$rLev0HX15rWPzp8j928yG.2uL9DGWYLcr8fxBnJmYyaeFPogxdiI2', 1733445270, 1733445270, 'MRE', 250, 1000, '127.0.0.1', true);"
 
 # DUMMY GAMES
 for i in $(seq 1 50);
