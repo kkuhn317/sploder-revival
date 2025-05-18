eadf0a388a9d85b27c29dc9e6844d76e522d19a0
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-05-18 15:16:27 +0530

    Add better text formatting

--- src/repositories/challengesrepository.php
+++ src/repositories/challengesrepository.php
@@ -130,13 +130,19 @@ class ChallengesRepository implements IChallengesRepository
             $minutes = floor($challenge / 60);
             $seconds = $challenge % 60;
             $result = "Win in less than";
-            if ($minutes > 0) {
-                $result .= " {$minutes} mins";
+
+            if ($minutes > 0 && $seconds == 0) {
+                $unit = $minutes == 1 ? "minute" : "minutes";
+                $result .= " {$minutes} {$unit}";
+            } elseif ($minutes > 0 && $seconds > 0) {
+                $minUnit = $minutes == 1 ? "min" : "mins";
+                $secUnit = $seconds == 1 ? "sec" : "secs";
+                $result .= " {$minutes} {$minUnit} {$seconds} {$secUnit}";
+            } elseif ($minutes == 0 && $seconds > 0) {
+                $unit = $seconds == 1 ? "second" : "seconds";
+                $result .= " {$seconds} {$unit}";
             }
-            if ($seconds > 0) {
-                if ($minutes > 0) $result .= " ";
-                $result .= "{$seconds} secs";
-            }
+
             return trim($result);
         } else {
             return "Score at least {$challenge} points";
