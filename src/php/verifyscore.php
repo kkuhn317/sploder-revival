43bc16d9905848a887acb8f47e30ceabb036d4c4
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-06-28 22:15:33 +0530

    Add stub verifyscore

---
+++ src/php/verifyscore.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+This file is PROPRIETARY and NOT LICENSED UNDER AGPL.
+You may modify it for your own use after reverse-engineering the checks used in the original player.
+If you manage to reverse-engineer the checks, please do not share the code publicly or with anyone else.
+This is to prevent hackers from easily bypassing the checks used in the original player.
+I know, security through obscurity is not a good idea, but this is the best I can do.
+*/
+
+function verifyScore($hash, $pubkey, $gtm, $w, $id)
+{
+    return true;
+}
