931e52f65dc268f152abcf38e760b1be2a08660d
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-07-31 15:42:31 +0530

    Fix session error in games search page

--- src/games/search.php
+++ src/games/search.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 require_once(__DIR__ . '/../services/GameListRenderService.php');
 require_once(__DIR__ . '/../repositories/repositorymanager.php');
 require_once('../content/taglister.php');
