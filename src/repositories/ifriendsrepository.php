1f2a074a97eb0acc9557475d4cc3fbd939b48d95
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-08-02 19:46:18 +0530

    Show the friends list on the member page

--- src/repositories/ifriendsrepository.php
+++ src/repositories/ifriendsrepository.php
@@ -18,4 +18,22 @@ interface IFriendsRepository
      * Views all friend requests for a user
      */
     public function setAllFriendsAsViewed(int $userId): void;
+
+    /**
+     * Gets the bested friends for a user, ordered by most recent first
+     * 
+     * @param string $username The username to get bested friends for
+     * @param int $limit Maximum number of friends to return
+     * @return array Array of friend records containing user1 and user2 fields
+     */
+    public function getBestedFriends(string $username, int $limit = 30): array;
+
+    /**
+     * Gets the regular (non-bested) friends for a user, ordered by most recent first
+     * 
+     * @param string $username The username to get friends for
+     * @param int $limit Maximum number of friends to return
+     * @return array Array of friend records containing user1 and user2 fields
+     */
+    public function getAcceptedFriends(string $username, int $limit): array;
 }
