99e92cdb03e6e09a6a6d29a0315e2738dafbb90d
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-08-06 21:44:20 +0530

    Initial commit

---
+++ src/content/disablemobile.php
@@ -0,0 +1,9 @@
+<?php
+// Check if the visitor is on a mobile device
+// Credits to whoever I stole this from... I honestly forgot
+if (preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo
+|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i"
+, $_SERVER["HTTP_USER_AGENT"])) {
+header('Location: /error_pages/mob.php');
+}
+?>
