96f987250a0a9a4f6081be6684b78d4695b22ae5
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-08-16 22:17:11 +0530

    Remove redundant comment

--- src/graphics/index.php
+++ src/graphics/index.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 require_once('../repositories/repositorymanager.php');
-// Get repository and service
 $graphicsRepository = RepositoryManager::get()->getGraphicsRepository();
 $total = $graphicsRepository->getTotalPublicGraphics();
 
