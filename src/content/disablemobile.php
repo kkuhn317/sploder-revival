3acf8b22fd73806f7606bbc55c1d6d881b2ef9d7
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-08-18 23:39:04 +0530

    Update disablemobile.php

--- src/content/disablemobile.php
+++ src/content/disablemobile.php
@@ -3,8 +3,8 @@
 $pageExecutionStartTime = microtime(true);
 // Check if the visitor is on a mobile device
 // Credits to whoever I stole this from... I honestly forgot
-if (preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo
-|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i"
-, $_SERVER["HTTP_USER_AGENT"])) {
-header('Location: /error_pages/mob.php');
+if (!preg_match("/Googlebot/i", $user_agent) &&
+    preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $user_agent)) {  
+    header('Location: /error_pages/mob.php');
+    exit();
 }
