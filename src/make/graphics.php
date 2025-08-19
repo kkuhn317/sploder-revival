7f1f8fce4a5c6b6fa9c467eafdd0d35dce2074e0
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-08-19 21:48:24 +0530

    Remove unneeded functions in graphics HTML

--- src/make/graphics.php
+++ src/make/graphics.php
@@ -17,111 +17,12 @@
 
         var page_start_time = (new Date()).getTime();
 
-
-
-        var popUpWin = 0;
-
-        function popUpWindow(URLStr, left, top, width, height)
-
-        {
-
-
-
-            if (popUpWin)
-
-            {
-
-                if (!popUpWin.closed) popUpWin.close();
-
-            }
-
-            popUpWin = open(URLStr, '_blank',
-                'toolbar=no,location=no,directories=no,status=no,menubar=no,scrollbars=yes,resizable=yes,copyhistory=yes,width=' +
-                width + ',height=' + height + ',left=' + left + ', top=' + top + ',screenX=' + left + ',screenY=' +
-                top + '');
-
-        }
-
-
-
-
         function getPhotos() {
 
-            popUpWindow("php/uploadform.php?PHPSESSID=", Math.floor(screen.width / 2) - 275, Math.floor(screen.height / 2) -
-                225, 550, 450);
+            //popUpWindow("php/uploadform.php?PHPSESSID=", Math.floor(screen.width / 2) - 275, Math.floor(screen.height / 2) -
+            //    225, 550, 450);
 
         }
-
-
-
-        function launchHelp() {
-
-            popUpWindow("help_inline.php?PHPSESSID=", Math.floor(screen.width / 2) - 275, Math.floor(screen.height / 2) -
-                225, 550, 450);
-
-        }
-
-
-
-        function playMovie(movieID, userID, creationDate, wide) {
-
-            if ((wide == null) | (wide < 660)) {
-
-                wide = 660;
-
-            } else if (wide > screen.width) {
-
-                wide = screen.width - 20;
-
-            }
-
-            popUpWindow("/php/player.php?PHPSESSID=&m=" + movieID + "&u=" + userID + "&c=" + creationDate, Math.floor(screen
-                .width / 2) - (wide / 2), Math.floor(screen.height / 2) - 200, wide, 400);
-
-        }
-
-
-
-        function playPubMovie(pubkey, wide) {
-
-            if ((wide == null) | (wide < 660)) {
-
-                wide = 660;
-
-            } else if (wide > screen.width) {
-
-                wide = screen.width - 20;
-
-            }
-
-            popUpWindow("/publish.php?PHPSESSID=&s=" + pubkey + "#kickdown", Math.floor(screen.width / 2) - (wide / 2), Math
-                .floor(screen.height / 2) - 270, wide, 540);
-
-        }
-
-
-
-        function updateMovie(value) {
-
-            var InternetExplorer = navigator.appName.indexOf("Microsoft") != -1;
-
-            if (InternetExplorer) {
-
-                document.creator.SetVariable("/browsermanager:callvalue", value);
-
-                document.creator.TPlay("browsermanager");
-
-            } else {
-
-                document.creator.SetVariable("/browsermanager:callvalue", value);
-
-                document.creator.TPlay("browsermanager");
-
-            }
-
-        }
-
-
 
 
         <?php if (!isset($_SESSION['username'])) { ?>
