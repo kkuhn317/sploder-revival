0d2caf2492b0ab53ae719c2bd8600d823e3006b2
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-09-10 20:12:10 +0530

    Update multiplayer.php

--- src/games/multiplayer.php
+++ src/games/multiplayer.php
@@ -59,6 +59,8 @@
 
                 default:
                     var room = e.data;
+                    // Print whatever is received
+                    console.log("message received: " + e.data);
                     if (room) window.location.href="/games/multiplayer.php?room=" + room;
                 break;
 				
