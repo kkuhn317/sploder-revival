92335b05bad41f59077a7914184fcc02e3a7130c
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-09-15 18:13:19 +0530

    Revert "Fix URL mismatch between browser and launcher"
    This reverts commit d101c0da7eb8a727e36688adce264edce5e8fb59.

--- src/php/url.php
+++ src/php/url.php
@@ -3,7 +3,7 @@
 $url = $_POST["url"];
 require_once(__DIR__ . '/../config/env.php');
 $domainName = getenv("DOMAIN_NAME");
-$domainNameNoWww = preg_replace('/^www\./', '', $domainName);
+$domain = substr($url, 0, strlen($domainName));
 $back = str_replace("&urlerr=1", "", str_replace("&err404=1", "", $_POST["back"]));
 
 // Extract the file path from the URL
@@ -11,18 +11,15 @@ $parsedUrl = parse_url($url);
 $file = $parsedUrl['path'];
 $exists = file_exists(__DIR__."/../".$file);
 
-$host = isset($parsedUrl['host']) ? $parsedUrl['host'] : '';
-$hostNoWww = preg_replace('/^www\./', '', $host);
-
 if (!$exists) {
     $err404 = true;
 } else {
     $err404 = false;
 }
 
-if (($host == $domainName || $hostNoWww === $domainNameNoWww) && ($err404 == false)) {
+if (($domain == $domainName) && ($err404 == false)) {
     header("Location: " . $url);
-} elseif ($host !== $domainName && $hostNoWww !== $domainNameNoWww) {
+} elseif ($domain !== $domainName) {
     header("Location: " . $back . "&urlerr=1");
 } elseif ($err404) {
     header("Location: " . $back . "&errload=1");
