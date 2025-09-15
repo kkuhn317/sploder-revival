d101c0da7eb8a727e36688adce264edce5e8fb59
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-09-15 18:09:47 +0530

    Fix URL mismatch between browser and launcher

--- src/php/url.php
+++ src/php/url.php
@@ -3,7 +3,7 @@
 $url = $_POST["url"];
 require_once(__DIR__ . '/../config/env.php');
 $domainName = getenv("DOMAIN_NAME");
-$domain = substr($url, 0, strlen($domainName));
+$domainNameNoWww = preg_replace('/^www\./', '', $domainName);
 $back = str_replace("&urlerr=1", "", str_replace("&err404=1", "", $_POST["back"]));
 
 // Extract the file path from the URL
@@ -11,15 +11,18 @@ $parsedUrl = parse_url($url);
 $file = $parsedUrl['path'];
 $exists = file_exists(__DIR__."/../".$file);
 
+$host = isset($parsedUrl['host']) ? $parsedUrl['host'] : '';
+$hostNoWww = preg_replace('/^www\./', '', $host);
+
 if (!$exists) {
     $err404 = true;
 } else {
     $err404 = false;
 }
 
-if (($domain == $domainName) && ($err404 == false)) {
+if (($host == $domainName || $hostNoWww === $domainNameNoWww) && ($err404 == false)) {
     header("Location: " . $url);
-} elseif ($domain !== $domainName) {
+} elseif ($host !== $domainName && $hostNoWww !== $domainNameNoWww) {
     header("Location: " . $back . "&urlerr=1");
 } elseif ($err404) {
     header("Location: " . $back . "&errload=1");
