d56c407d5690b843e40e806cbe87216c1a891aff
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-09-20 14:15:52 +0530

    Remove archive.org references from the online list

--- src/php/onlinelist.php
+++ src/php/onlinelist.php
@@ -8,22 +8,9 @@ $pagechange = $time - 900;
 $userRepository = RepositoryManager::get()->getUserRepository();
 $members = $userRepository->getOnlineMembers();
 $total = count($members);
+// Set header as javascript
+header('Content-Type: text/javascript');
 ?>
-var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init &&
-self._wb_wombat.local_init(name)) || self[name]; };
-if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
-{
-let window = _____WB$wombat$assign$function_____("window");
-let self = _____WB$wombat$assign$function_____("self");
-let document = _____WB$wombat$assign$function_____("document");
-let location = _____WB$wombat$assign$function_____("location");
-let top = _____WB$wombat$assign$function_____("top");
-let parent = _____WB$wombat$assign$function_____("parent");
-let frames = _____WB$wombat$assign$function_____("frames");
-let opener = _____WB$wombat$assign$function_____("opener");
-
-
-
 try {
 
 var net_result = ' <div class="users_online"><ul><?php
@@ -62,5 +49,4 @@ c.innerHTML = net_result;
 } catch (err) {
 
 
-}
 }
