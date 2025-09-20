4c41b5062a7e54d27e0ecf53b134b89c3f88a9cd
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-09-20 16:00:06 +0530

    Update versionstring.php

--- src/content/versionstring.php
+++ src/content/versionstring.php
@@ -7,5 +7,5 @@ $peakMemoryUsage = number_format(memory_get_peak_usage() / 1024 / 1024, 2) . " M
 <div style="position: fixed; right: 0.7vw; bottom: 0.7vw; font-size: 0.8vw; opacity: 50%; z-index: 1000; text-align: right; cursor: default; user-select: none;">
     <span title="Page Execution Time"><?= $pageExecutionTime ?></span><br>
     <span title="Peak Memory Usage"><?= $peakMemoryUsage ?></span><br>
-    <span title="Version">alpha v1.0</span>
+    <span title="Version">alpha v1.1</span>
 </div>
