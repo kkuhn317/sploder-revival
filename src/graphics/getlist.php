1773966cd50d87a0c95c1dfd6de31064f96f67a2
Malware <85979829+Sploder-Saptarshi@users.noreply.github.com>
2025-09-24 20:08:55 +0530

    Try fix graphic tags

--- src/graphics/getlist.php
+++ src/graphics/getlist.php
@@ -3,8 +3,8 @@ require_once '../content/initialize.php';
 
 header('Content-Type: application/xml');
 session_start();
-$start = $_GET['start']; // Offset from this value
-$num = $_GET['num']; // LIMIT to this value
+$start = $_GET['start'] ?? 0; 
+$num = $_GET['num'] ?? 12; 
 
 require('../database/connect.php');
 
@@ -18,42 +18,64 @@ $params = [
     ':start' => $start
 ];
 
-if ($_GET['userid'] == $_SESSION['userid'] && !isset($_GET['searchmode'])) {
-    $clause = isset($_GET['published']) ? "ispublished=true" : "1=1";
+
+$isLoggedIn = isset($_SESSION['userid']) && $_GET['userid'] == $_SESSION['userid'];
+
+
+if ($isLoggedIn && !isset($_GET['searchmode'])) {
+    $clause = "1=1";
     $params[':userid'] = $_SESSION['userid'];
     $clause .= " AND userid=:userid";
     $order = "id";
-} elseif($_GET['userid']==0 || isset($_GET['searchmode'])) {
-    if(isset($_GET['searchmode'])){
+} elseif ($_GET['userid'] == 0 || isset($_GET['searchmode'])) {
+    if (isset($_GET['searchmode'])) {
         $searchmode = $_GET['searchmode'];
         $searchterm = $_GET['searchterm'];
-        if($searchmode == "users"){
-            // Search by username according to corresponding userid
+        if ($searchmode == "users") {
+            
             $qs = "SELECT userid FROM members WHERE username=:username";
             $userid = $db->queryFirstColumn($qs, 0, [':username' => $searchterm]);
-            $clause .= " AND userid=:userid";
-            $params[':userid'] = $userid;
-        } else {
-            // Search by tags
-            $qs = "SELECT g_id FROM graphic_tags WHERE SIMILARITY(tag, :tag) > 0.3";
-            $g_ids = $db->queryFirst($qs, [':tag' => $searchterm]);
-            // Remove duplicate keys in g_ids
-            $g_ids = array_unique($g_ids);
-            if (!empty($g_ids)) {
-                $g_ids = array_values($g_ids); // Extract values from associative array
-                $clause .= " AND id IN (".implode(",", array_map('intval', $g_ids)).")";
-                $extrainfo .= ", (SELECT username FROM members WHERE userid=graphics.userid) AS username";
+            if ($userid) {
+                
+                $clause .= " AND userid=:userid";
+                $params[':userid'] = $userid;
             } else {
-                $clause .= " AND 1=0"; // No results found
+                
+                $clause .= " AND 1=0";
+            }
+        } else {
+            
+            
+            $qs = "SELECT g_id FROM graphic_tags WHERE SIMILARITY(tag, :tag) > 0.3";
+            $g_ids = $db->queryFirstColumn($qs, 0, [':tag' => $searchterm]);
+            
+            
+            if (!empty($g_ids)) {
+                $g_ids = array_unique($g_ids); 
+                $clause .= " AND id IN (" . implode(",", array_map('intval', $g_ids)) . ")";
+            } else {
+                
+                $clause .= " AND 1=0";
             }
         }
-    } else{
+    } else {
+        
         $extrainfo .= ", (SELECT username FROM members WHERE userid=graphics.userid) AS username";
     }
-    $extrainfo .= ", (SELECT COUNT(*) FROM graphic_likes WHERE graphic_likes.g_id=graphics.id) AS likes"; // Use later to grab likes
+
+    
+    $extrainfo .= ", (SELECT COUNT(*) FROM graphic_likes WHERE graphic_likes.g_id=graphics.id) AS likes";
+    
+    
+    if ($isLoggedIn && isset($_GET['searchmode'])) {
+        $clause = "userid=:userid AND " . $clause;
+        $params[':userid'] = $_SESSION['userid'];
+        $clause = str_replace("ispublished=true AND isprivate=false", "1=1", $clause);
+    }
 }
 
-$graphics_qs = "SELECT id,version$extrainfo FROM graphics WHERE $clause ORDER BY $order DESC LIMIT :num OFFSET :start";
+
+$graphics_qs = "SELECT id, version$extrainfo FROM graphics WHERE $clause ORDER BY $order DESC LIMIT :num OFFSET :start";
 $graphicsData = $db->query($graphics_qs, $params);
 $graphicsData = array_map(function($graphic) {
     $filteredGraphic = array_filter($graphic, function($key) {
@@ -63,7 +85,7 @@ $graphicsData = array_map(function($graphic) {
 }, $graphicsData);
 unset($params[':num']);
 unset($params[':start']);
-$total = $db->queryFirstColumn("SELECT COUNT(*) FROM graphics WHERE $clause", 0,$params);
+$total = $db->queryFirstColumn("SELECT COUNT(*) FROM graphics WHERE $clause", 0, $params);
 
 $xml = new SimpleXMLElement('<graphics/>');
 $xml->addAttribute('start', $start);
@@ -76,6 +98,5 @@ foreach ($graphicsData as $graphic) {
         $g->addAttribute($key, $value);
     }
 }
-
 
 echo $xml->asXML();
